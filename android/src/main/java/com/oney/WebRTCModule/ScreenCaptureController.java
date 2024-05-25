25fddc588ad99e6c1b00dc2c5cc3a461dee541e5
owaisansarii <owaisansari1112@gmail.com>
2024-05-25 15:54:32 +0530

    Add isCapturing flag to control capture format changes and OrientationEventListener is disabled during disposal.

--- android/src/main/java/com/oney/WebRTCModule/ScreenCaptureController.java
+++ android/src/main/java/com/oney/WebRTCModule/ScreenCaptureController.java
@@ -25,6 +25,8 @@ public class ScreenCaptureController extends AbstractVideoCaptureController {
 
     private final Context context;
 
+    private boolean isCapturing;
+
     public ScreenCaptureController(Context context,
                                    int width,
                                    int height,
@@ -35,23 +37,28 @@ public class ScreenCaptureController extends AbstractVideoCaptureController {
 
         this.context = context;
 
+        this.isCapturing = false;
+
         this.orientationListener = new OrientationEventListener(context) {
             @Override
             public void onOrientationChanged(int orientation) {
-                DisplayMetrics displayMetrics = DisplayUtils.getDisplayMetrics((Activity) context);
-                final int width = displayMetrics.widthPixels;
-                final int height = displayMetrics.heightPixels;
+                if (isCapturing) {
+                    DisplayMetrics displayMetrics = DisplayUtils.getDisplayMetrics((Activity) context);
+                    final int width = displayMetrics.widthPixels;
+                    final int height = displayMetrics.heightPixels;
 
-                // Pivot to the executor thread because videoCapturer.changeCaptureFormat runs in the main
-                // thread and may deadlock.
-                ThreadUtils.runOnExecutor(() -> {
-                    try {
-                        videoCapturer.changeCaptureFormat(width, height, DEFAULT_FPS);
-                    } catch (Exception ex) {
-                        // We ignore exceptions here. The video capturer runs on its own
-                        // thread and we cannot synchronize with it.
-                    }
-                });
+                    // Pivot to the executor thread because videoCapturer.changeCaptureFormat runs in the main
+                    // thread and may deadlock.
+                    ThreadUtils.runOnExecutor(() -> {
+                        try {
+                            videoCapturer.changeCaptureFormat(width, height, DEFAULT_FPS);
+                        } catch (Exception ex) {
+                             Log.e(TAG, "Error changing capture format", ex);
+                            // We ignore exceptions here. The video capturer runs on its own
+                            // thread and we cannot synchronize with it.
+                        }
+                    });
+                }
             }
         };
 
@@ -63,6 +70,7 @@ public class ScreenCaptureController extends AbstractVideoCaptureController {
     @Override
     public void dispose() {
         MediaProjectionService.abort(context);
+        orientationListener.disable();
         super.dispose();
     }
 
@@ -75,7 +83,7 @@ public class ScreenCaptureController extends AbstractVideoCaptureController {
                         Log.w(TAG, "Media projection stopped.");
                         orientationListener.disable();
                         stopCapture();
-
+                        isCapturing = false;
                         if (capturerEventsListener != null) {
                             capturerEventsListener.onCapturerEnded();
                         }
